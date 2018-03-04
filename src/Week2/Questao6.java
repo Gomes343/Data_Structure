ff9f7a6de2dd73ed951ac751e64e62e2b4916366
Murilo Gomes Teixeira <murilogomes@hotmail.com.br>
2018-03-03 21:47:24 -0300

    Finished.

--- src/Week2/Questao6.java
+++ src/Week2/Questao6.java
@@ -3,41 +3,32 @@ package Week2;
 public class Questao6 {
 
 
-public int MenorValor(int v[],int n){
-if(n>=0){
+    public int MenorValor(int v[],int n){
+    if(n>0){
+        int x = MenorValor(v,n-1);
+        if(x < v[n]){
+        return x;
+        }
 
-if(v[n] < MenorValor(v,n-1)){
+        return v[n];
 
-return v[n];
-
-}else{
-
-return MenorValor(v,n-1);
-
-} 
-}
-
-//Parte que pode ser excluida se o método for void
-if(v[1] < v[0]){
-return v[1];
-}else{
-return v[0];
-} //porém, ela trata o fim do critério de parada.
+        }
+    return v[n];
+    }
 
 
-}
 
-public static void main(String[]args){
+    public static void main(String[]args){
 
-Questao6 menor = new Questao6();
-int v[] = new int[5];
-v[0] = 40;
-v[1] = 99;
-v[2] = 10;
-v[3] = 3;
-v[4] = 9; 
-
-System.out.println(menor.MenorValor(v, 4));
-}
+    Questao6 menor = new Questao6();
+    int v[] = new int[5];
+    v[0] = 40;
+    v[1] = 99;
+    v[2] = 1;
+    v[3] = 3;
+    v[4] = 9; 
+    
+    System.out.println(menor.MenorValor(v, 4));
+    }
 }
 //Escreva uma função recursiva que retorne o menor elemento em um vetor
